89bd4797be53c94144d022f09b3c5b8c30fbbbb8
BlendyDev <github@blendy.dev>
2025-07-30 23:37:01 +0200

    grab player to start

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,35 +1,29 @@
 extends CharacterBody2D
 
-@export var mouse_pos: Vector2
-@export var overlapping_count: int
 @onready var offset :Vector2 = (get_viewport().size/2) 
 @export var last_pos : Vector2
 @export var speed: float
+var playing := false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	mouse_pos = get_viewport().get_mouse_position()
-	last_pos = mouse_pos
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	
 	pass # Replace with function body.
+
+func bind_to_player():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	playing = true
 
 func _input(e: InputEvent):
 	match e.get_class():
 		"InputEventMouseMotion":
+			if !playing: return
 			velocity = Vector2(
 				min(offset.x, max(-offset.x, position.x + e.relative.x))-position.x,
 				min(offset.y, max(-offset.y, position.y + e.relative.y))-position.y
 			)*speed
 			move_and_slide()
-			
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	
-	
-	mouse_pos = get_viewport().get_mouse_position()
-	
-	
-	if (overlapping_count == 0):
-		last_pos = position
+	if ((get_viewport().get_mouse_position()-offset).distance_to(position) < 30 and !playing):
+		bind_to_player()
 	pass
