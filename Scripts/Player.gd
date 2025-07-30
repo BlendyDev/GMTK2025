485d1cb16917e7ddcf4d3510a29bd8207d0726b1
BlendyDev <github@blendy.dev>
2025-07-31 01:29:50 +0200

    trail

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,11 +1,16 @@
 extends CharacterBody2D
 
 @onready var offset :Vector2 = (get_viewport().size/2) 
-@export var last_pos : Vector2
 @export var speed: float
+@export var trail: Line2D
+@export var trail_points_per_second: int
 var playing := false
+@onready var last_trail_point_timestamp : int = Time.get_ticks_msec()
+@export var max_time_sec: float
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	last_trail_point_timestamp = Time.get_ticks_msec()
 	pass # Replace with function body.
 
 func bind_to_player():
@@ -24,6 +29,15 @@ func _input(e: InputEvent):
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	if (Time.get_ticks_msec() - last_trail_point_timestamp > 1000/trail_points_per_second):
+		var n = floor((Time.get_ticks_msec() - last_trail_point_timestamp) / (1000/trail_points_per_second))
+		for i in range(n):
+			if (trail.points.size() > max_time_sec * trail_points_per_second):
+				trail.remove_point(0)
+			trail.add_point(position)
+		last_trail_point_timestamp += n* (1000/trail_points_per_second)
+		pass
+	
 	if ((get_viewport().get_mouse_position()-offset).distance_to(position) < 30 and !playing):
 		bind_to_player()
 	pass
