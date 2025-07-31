0cda0326f60e8155ad456d19abdc2d8f5345442f
BlendyDev <github@blendy.dev>
2025-07-31 02:12:36 +0200

    circulos

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -7,6 +7,8 @@ extends CharacterBody2D
 var playing := false
 @onready var last_trail_point_timestamp : int = Time.get_ticks_msec()
 @export var max_time_sec: float
+@export var min_distance_to_oldest_points: float
+var cleared_points = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -27,6 +29,14 @@ func _input(e: InputEvent):
 			)*speed
 			move_and_slide()
 
+func min_distance_to_last_points(rate: float) -> float:
+	var distance: float = INF
+	for i in range (min(trail.points.size(), ceil(rate*max_time_sec*trail_points_per_second))):
+		var current_dist := position.distance_to(trail.points.get(i))
+		if (current_dist < distance):
+			distance = current_dist
+	return distance
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if (Time.get_ticks_msec() - last_trail_point_timestamp > 1000/trail_points_per_second):
@@ -37,6 +47,12 @@ func _process(delta: float) -> void:
 			trail.add_point(position)
 		last_trail_point_timestamp += n* (1000/trail_points_per_second)
 		pass
+	var distance = min_distance_to_last_points(0.1)
+	if distance < min_distance_to_oldest_points and !cleared_points:
+		trail.clear_points()
+		cleared_points = true
+	if distance * 1.2 > min_distance_to_oldest_points:
+		cleared_points = false
 	
 	if ((get_viewport().get_mouse_position()-offset).distance_to(position) < 30 and !playing):
 		bind_to_player()
