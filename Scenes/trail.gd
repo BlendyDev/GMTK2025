ae1c2065bff52a71d0df746242f5f682d0822a38
BlendyDev <github@blendy.dev>
2025-07-31 14:31:05 +0200

    trail.gd script and framerate independent movement

---
+++ Scenes/trail.gd
@@ -0,0 +1,85 @@
+extends Area2D
+
+@export var trail: Line2D
+@export var trail_points_per_second: int
+@onready var last_trail_point_timestamp : int = Time.get_ticks_msec()
+@export var max_time_sec: float
+@export var min_distance_to_oldest_points: float
+var cleared_points = true
+@onready var last_circle_timestamp:= Time.get_ticks_msec()
+@export var circle_min_timeout_sec: float
+@export var player: Player
+@export var trail_collision: CollisionPolygon2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	last_trail_point_timestamp = Time.get_ticks_msec()
+	pass # Replace with function body.
+
+func find_closest_point(rate: float) -> Vector2:
+	var point: Vector2
+	var distance:float = INF
+	for i in range (min(trail.points.size(), ceil(rate*max_time_sec*trail_points_per_second))):
+		var current_point := trail.points.get(i)
+		var current_dist := player.position.distance_to(current_point)
+		if (current_dist < distance):
+			distance = current_dist
+			point = current_point
+	return point
+
+func spawn_circle(closest_point: Vector2):
+	$"../Trail/AudioStreamPlayer2D".play()
+	var fade_out_trail = Line2D.new()
+	fade_out_trail.antialiased = true
+	fade_out_trail.width = 3.0
+	fade_out_trail.points = trail.points.duplicate()
+	for i in range(trail.points.size()):
+		if (fade_out_trail.points.get(0) == closest_point): break
+		fade_out_trail.remove_point(0)
+		
+	fade_out_trail.add_point(closest_point)
+	var area = Area2D.new()
+	fade_out_trail.add_child(area)
+	var initial_polygon :PackedVector2Array = fade_out_trail.points.duplicate()
+	var polygons := Geometry2D.merge_polygons(initial_polygon, PackedVector2Array([]))
+	for polygon in polygons:
+		var collision = CollisionPolygon2D.new()
+		collision.polygon = polygon
+		area.add_child(collision)
+	
+	var tween = get_tree().create_tween()
+	tween.tween_property(fade_out_trail, "default_color", Color.from_rgba8(255, 255, 255, 0), 1)
+	tween.tween_callback(fade_out_trail.queue_free)
+	get_tree().current_scene.add_child(fade_out_trail)
+	
+	
+	trail.clear_points()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if (!player.playing): return
+	if (trail.points.size() > 1):
+		pass
+	if (Time.get_ticks_msec() - last_trail_point_timestamp > 1000.0/trail_points_per_second):
+		var n = floor((Time.get_ticks_msec() - last_trail_point_timestamp) / (1000.0/trail_points_per_second))
+		var points: Array[Vector2]
+		var last_position: Vector2
+		if (trail.points.size() == 0): last_position = player.position
+		else: last_position = trail.points.get(trail.points.size()-1)
+		var direction = player.position - last_position
+		for i in range(n):
+			if (trail.points.size() > max_time_sec * trail_points_per_second):
+				trail.remove_point(0)
+			trail.add_point(last_position + direction* (i+1)/n)
+		last_trail_point_timestamp += n* (1000.0/trail_points_per_second)
+		pass
+	var closest_point = find_closest_point(0.2)
+	var distance = player.position.distance_to(closest_point)
+	if (distance < min_distance_to_oldest_points and !cleared_points 
+	and Time.get_ticks_msec() - last_circle_timestamp > 1000*circle_min_timeout_sec):
+		last_circle_timestamp = Time.get_ticks_msec()
+		spawn_circle(closest_point)
+		cleared_points = true
+	if distance > min_distance_to_oldest_points * 3:
+		cleared_points = false
+	pass
--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,20 +1,15 @@
 extends CharacterBody2D
 
+class_name Player
+
 @onready var offset :Vector2 = (get_viewport().size/2) 
 @export var speed: float
-@export var trail: Line2D
-@export var trail_points_per_second: int
 var playing := false
-@onready var last_trail_point_timestamp : int = Time.get_ticks_msec()
-@export var max_time_sec: float
-@export var min_distance_to_oldest_points: float
-var cleared_points = true
-@onready var last_circle_timestamp:= Time.get_ticks_msec()
-@export var circle_min_timeout_sec: float
+var mouse_motion:= Vector2.ZERO
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	last_trail_point_timestamp = Time.get_ticks_msec()
 	pass # Replace with function body.
 
 func bind_to_player():
@@ -26,82 +21,25 @@ func _input(e: InputEvent):
 	match e.get_class():
 		"InputEventMouseMotion":
 			if !playing: return
-			velocity = Vector2(
-				min(offset.x, max(-offset.x, position.x + e.relative.x))-position.x,
-				min(offset.y, max(-offset.y, position.y + e.relative.y))-position.y
-			)*speed
-			move_and_slide()
-
-func find_closest_point(rate: float) -> Vector2:
-	var point: Vector2
-	var distance:float = INF
-	for i in range (min(trail.points.size(), ceil(rate*max_time_sec*trail_points_per_second))):
-		var current_point := trail.points.get(i)
-		var current_dist := position.distance_to(current_point)
-		if (current_dist < distance):
-			distance = current_dist
-			point = current_point
-	return point
-
-
-func spawn_circle(closest_point: Vector2):
-	$"../finalTrail/AudioStreamPlayer2D".play()
-	var fade_out_trail = Line2D.new()
-	fade_out_trail.antialiased = true
-	fade_out_trail.width = 3.0
-	fade_out_trail.points = trail.points.duplicate()
-	for i in range(trail.points.size()):
-		if (fade_out_trail.points.get(0) == closest_point): break
-		fade_out_trail.remove_point(0)
-		
-	fade_out_trail.add_point(closest_point)
-	var area = Area2D.new()
-	fade_out_trail.add_child(area)
-	var initial_polygon :PackedVector2Array = fade_out_trail.points.duplicate()
-	var polygons := simplify_polygon(initial_polygon)
-	print(polygons.size())
-	for polygon in polygons:
-		var collision = CollisionPolygon2D.new()
-		collision.polygon = polygon
-		area.add_child(collision)
-	
-	var tween = get_tree().create_tween()
-	tween.tween_property(fade_out_trail, "default_color", Color.from_rgba8(255, 255, 255, 0), 1)
-	tween.tween_callback(fade_out_trail.queue_free)
-	get_tree().current_scene.add_child(fade_out_trail)
-	
-	
-	trail.clear_points()
-
-func simplify_polygon(polygon: PackedVector2Array) -> Array[PackedVector2Array]:
-	return Geometry2D.merge_polygons(polygon, PackedVector2Array([]))
+			mouse_motion = e.relative
+			
+			
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	velocity = Vector2(
+		min(offset.x, max(-offset.x, position.x + mouse_motion.x))-position.x,
+		min(offset.y, max(-offset.y, position.y + mouse_motion.y))-position.y
+	)*speed * (1/delta)
+	mouse_motion = Vector2.ZERO
+	print(velocity)
+	print(delta)
+	move_and_slide()
 	if (Input.is_key_pressed(KEY_SPACE)):
 		pass
 		
 	if !playing:
 		return
-	if (Time.get_ticks_msec() - last_trail_point_timestamp > 1000.0/trail_points_per_second):
-		var n = floor((Time.get_ticks_msec() - last_trail_point_timestamp) / (1000.0/trail_points_per_second))
-		for i in range(n):
-			if (trail.points.size() > max_time_sec * trail_points_per_second):
-				trail.remove_point(0)
-			trail.add_point(position)
-		last_trail_point_timestamp += n* (1000.0/trail_points_per_second)
-		pass
-	var closest_point = find_closest_point(0.2)
-	var distance = position.distance_to(closest_point)
-	if (distance < min_distance_to_oldest_points and !cleared_points 
-	and Time.get_ticks_msec() - last_circle_timestamp > 1000*circle_min_timeout_sec):
-		last_circle_timestamp = Time.get_ticks_msec()
-		spawn_circle(closest_point)
-		cleared_points = true
-	if distance > min_distance_to_oldest_points * 3:
-		cleared_points = false
-	
-	
 	pass
 
 
