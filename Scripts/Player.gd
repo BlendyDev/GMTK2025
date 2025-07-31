b1371fce465ec709f4977d27718d6d2f39fdb41b
BlendyDev <github@blendy.dev>
2025-07-31 03:35:00 +0200

    fix: circle proper closing & min circle timeout

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -9,6 +9,8 @@ var playing := false
 @export var max_time_sec: float
 @export var min_distance_to_oldest_points: float
 var cleared_points = true
+@onready var last_circle_timestamp:= Time.get_ticks_msec()
+@export var circle_min_timeout_sec: float
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -17,6 +19,7 @@ func _ready() -> void:
 
 func bind_to_player():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	print("playing!")
 	playing = true
 
 func _input(e: InputEvent):
@@ -29,22 +32,29 @@ func _input(e: InputEvent):
 			)*speed
 			move_and_slide()
 
-func min_distance_to_last_points(rate: float) -> float:
-	var distance: float = INF
+func closest_point(rate: float) -> Vector2:
+	var point: Vector2
+	var distance:float = INF
 	for i in range (min(trail.points.size(), ceil(rate*max_time_sec*trail_points_per_second))):
-		var current_dist := position.distance_to(trail.points.get(i))
+		var current_point := trail.points.get(i)
+		var current_dist := position.distance_to(current_point)
 		if (current_dist < distance):
 			distance = current_dist
-	return distance
+			point = current_point
+	return point
 
 
-func spawn_circle():
+func spawn_circle(closest_point: Vector2):
 	$"../finalTrail/AudioStreamPlayer2D".play()
 	var fade_out_trail = Line2D.new()
 	fade_out_trail.antialiased = true
 	fade_out_trail.width = 3.0
 	fade_out_trail.points = trail.points.duplicate()
-	fade_out_trail.add_point(trail.points.get(0))
+	for i in range(trail.points.size()):
+		if (fade_out_trail.points.get(0) == closest_point): break
+		fade_out_trail.remove_point(0)
+		
+	fade_out_trail.add_point(closest_point)
 	var tween = get_tree().create_tween()
 	tween.tween_property(fade_out_trail, "default_color", Color.from_rgba8(255, 255, 255, 0), 1)
 	tween.tween_callback(fade_out_trail.queue_free)
@@ -65,9 +75,12 @@ func _process(delta: float) -> void:
 			trail.add_point(position)
 		last_trail_point_timestamp += n* (1000.0/trail_points_per_second)
 		pass
-	var distance = min_distance_to_last_points(0.1)
-	if distance < min_distance_to_oldest_points and !cleared_points:
-		spawn_circle()
+	var closest_point = closest_point(0.2)
+	var distance = position.distance_to(closest_point)
+	if (distance < min_distance_to_oldest_points and !cleared_points 
+	and Time.get_ticks_msec() - last_circle_timestamp > 1000*circle_min_timeout_sec):
+		last_circle_timestamp = Time.get_ticks_msec()
+		spawn_circle(closest_point)
 		cleared_points = true
 	if distance > min_distance_to_oldest_points * 3:
 		cleared_points = false
