da8fa074d69a7da2e8d00db7911f4a58962c2373
BlendyDev <github@blendy.dev>
2025-07-31 02:50:13 +0200

    fadeout & sfx

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -36,24 +36,41 @@ func min_distance_to_last_points(rate: float) -> float:
 		if (current_dist < distance):
 			distance = current_dist
 	return distance
-	
+
+
+func spawn_circle():
+	$"../finalTrail/AudioStreamPlayer2D".play()
+	var fade_out_trail = Line2D.new()
+	fade_out_trail.antialiased = true
+	fade_out_trail.width = 3.0
+	fade_out_trail.points = trail.points.duplicate()
+	fade_out_trail.add_point(trail.points.get(0))
+	var tween = get_tree().create_tween()
+	tween.tween_property(fade_out_trail, "default_color", Color.from_rgba8(255, 255, 255, 0), 1)
+	tween.tween_callback(fade_out_trail.queue_free)
+	get_tree().current_scene.add_child(fade_out_trail)
+	trail.clear_points()
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if (Time.get_ticks_msec() - last_trail_point_timestamp > 1000/trail_points_per_second):
-		var n = floor((Time.get_ticks_msec() - last_trail_point_timestamp) / (1000/trail_points_per_second))
+	if ((get_viewport().get_mouse_position()-offset).distance_to(position) < 30 and !playing):
+		bind_to_player()
+	if !playing:
+		return
+	if (Time.get_ticks_msec() - last_trail_point_timestamp > 1000.0/trail_points_per_second):
+		var n = floor((Time.get_ticks_msec() - last_trail_point_timestamp) / (1000.0/trail_points_per_second))
 		for i in range(n):
 			if (trail.points.size() > max_time_sec * trail_points_per_second):
 				trail.remove_point(0)
 			trail.add_point(position)
-		last_trail_point_timestamp += n* (1000/trail_points_per_second)
+		last_trail_point_timestamp += n* (1000.0/trail_points_per_second)
 		pass
 	var distance = min_distance_to_last_points(0.1)
 	if distance < min_distance_to_oldest_points and !cleared_points:
-		trail.clear_points()
+		spawn_circle()
 		cleared_points = true
-	if distance * 1.2 > min_distance_to_oldest_points:
+	if distance > min_distance_to_oldest_points * 3:
 		cleared_points = false
 	
-	if ((get_viewport().get_mouse_position()-offset).distance_to(position) < 30 and !playing):
-		bind_to_player()
+	
 	pass
