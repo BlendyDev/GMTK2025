261162749ebbf5b0fadccbd1ec89a83eaeaa7d2a
BlendyDev <github@blendy.dev>
2025-08-01 18:36:06 +0200

    fix: level.tscn

--- Scripts/intro_cutscene.gd
+++ Scripts/intro_cutscene.gd
@@ -38,7 +38,6 @@ func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 	if anim_name == "showtext":
 		texted = false
 	else:
-		print(anim_name)
 		$VideoStreamPlayer.paused = true
 		get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
 
