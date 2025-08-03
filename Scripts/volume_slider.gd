1b4b1efe1d4a78ca789707c92df211335158d335
BlendyDev <github@blendy.dev>
2025-08-03 14:48:51 +0200

    slider texture basic impl

--- Scripts/volume_slider.gd
+++ Scripts/volume_slider.gd
@@ -1,16 +1,18 @@
 extends HSlider
 
-@export
-var bus_name: String
+@export var bus_name: String
 
 var bus_index: int
+@onready var bg: Control = $BG
 
 func _ready() -> void:
 	bus_index = AudioServer.get_bus_index(bus_name)
 	value_changed.connect(_on_value_changed)
 	value = db_to_linear(AudioServer.get_bus_volume_db(bus_index))
 
+
 func _on_value_changed(value: float) -> void:
+	bg.size = Vector2(10 + value*146.0, bg.size.y)
 	AudioServer.set_bus_volume_db(bus_index, linear_to_db(value))
 
 
